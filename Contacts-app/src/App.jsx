43dbb86aba0fab32718ac382ccb4ff0f1256b94d
Prakhar <prakharb56@gmail.com>
2023-05-24 05:16:59 +0530

    feat: sidenav , footer & Contacts

--- Contacts-app/src/App.jsx
+++ Contacts-app/src/App.jsx
@@ -3,23 +3,41 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { css } from "@emotion/react";
+import { Outlet } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-
-function Layout(){
-
-}
-  return (
-    <>
-    <main>
-      <aside></aside>
-      <section>comment</section>
-    </main>
-    </>
-  )
-
+function Sidenav() {
+ 
+return <aside>
+  <header>
+  </header>
+    <section>
+      list of contacts
+    </section>
+    <footer>Footer</footer>
+</aside>
 }
 
-export default App;
+function Contents(){
+  return(
+  <section>
+  <Outlet/>
+</section>
+)}
+
+  export function Layout(){
+
+    return (
+      <>
+      <main css={css`
+      display:grid;
+      grid-template-columns: minmax(250px ,25%) 1fr;
+      `}>
+        <Sidenav/> 
+        <Contents/>
+      </main>
+      </>
+    )
+  }
+
+
+
--- Contacts-app/src/main.jsx
+++ Contacts-app/src/main.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {Layout} from './App.jsx'
 import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    
-    <Route path='/' element={<Layout/>}>
-
-      <Route path='/contacts/:contactId' element={<div>contacts</div>}/>
+    <Route path="/" element={<Layout/>}>
+      <Route path="/contacts/:contactId" element={<div>contacts</div>}/>
     </Route>
-  
   )
-)
+);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
   <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
