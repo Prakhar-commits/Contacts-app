029ff68574b9b6c77906ff7390c8fa974217a171
Prakhar <prakharb56@gmail.com>
2023-05-24 06:00:32 +0530

    Styling :

--- Contacts-app/src/App.jsx
+++ Contacts-app/src/App.jsx
@@ -6,38 +6,52 @@ import { css } from "@emotion/react";
 import { Outlet } from "react-router-dom";
 
 function Sidenav() {
- 
-return <aside>
-  <header>
-  </header>
+  return (
+    <aside css={css`
+    border-right: 1px solid;
+    display: grid;
+    grid-template-rows: auto 1fr auto;`
+    }>
+      <header css={css`
+      border-bottom: 1px solid;
+      padding: 16px;`}>
+     <form css={css`
+     display: flex;
+     justify-content: space-between;`}>
+      <input type="text" />
+      <button>Search</button>
+     </form>
+      </header>
+      <section css={css`
+      padding:16px;`}>list of contacts</section>
+      <footer css={css`
+      border-top: 1px solid
+      padding: 16px`}>Footer</footer>
+    </aside>
+  );
+}
+  
+function Contents() {
+  return (
     <section>
-      list of contacts
+      <Outlet />
     </section>
-    <footer>Footer</footer>
-</aside>
+  );
 }
 
-function Contents(){
-  return(
-  <section>
-  <Outlet/>
-</section>
-)}
-
-  export function Layout(){
-
-    return (
-      <>
-      <main css={css`
-      display:grid;
-      grid-template-columns: minmax(250px ,25%) 1fr;
-      `}>
-        <Sidenav/> 
-        <Contents/>
+export function Layout() {
+  return (
+    <>
+      <main
+        css={css`
+          display: grid;
+          grid-template-columns: minmax(250px, 25%) 1fr;
+          height: 100%;
+        `}
+      >
+        <Sidenav />
+        <Contents />
       </main>
-      </>
-    )
-  }
-
-
-
+    </>
+  );
+}
