0f44bdfa3259075c865dc24d104aa452e8578fac
Prakhar <prakharb56@gmail.com>
2023-05-25 01:13:21 +0530

    updated

--- Contacts-app/src/App.jsx
+++ Contacts-app/src/App.jsx
@@ -1,7 +1,4 @@
 import { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { css } from "@emotion/react";
 import { Outlet } from "react-router-dom";
 import ContactsList from "./ContactsList";
